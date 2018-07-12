9557670b9fb97bf2af1013141f0fa4f89e348dff
carloshjoaquim <carloshj2011@gmail.com>
2018-07-12 20:29:37 -0300

    Incluindo login

---
+++ Estoque/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Estoque.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Estoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Logar(string login, string senha)
+        {
+            var dao = new UsuariosDAO();
+            var usuario = dao.Busca(login, senha);
+
+            if (usuario != null)
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
